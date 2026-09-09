15460b218f78f92421f1c409da8b16d9228688bc
allentran2606 <giahaot2606@gmail.com>
2026-09-09 20:19:34 +0700

    chore: initialize project

---
+++ src/main/java/iuh/wwwprogramming/WwwProgrammingApplication.java
@@ -0,0 +1,12 @@
+package iuh.wwwprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwProgrammingApplication.class, args);
+	}
+}
---
+++ src/test/java/iuh/wwwprogramming/WwwProgrammingApplicationTests.java
@@ -0,0 +1,11 @@
+package iuh.wwwprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
